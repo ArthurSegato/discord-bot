b951349372f1cfd54121fcc060ab1748da8facdf
Arthur Segato <arthursegato@protonmail.com>
2024-10-11 22:46:54 +0100

    Refactor command handler

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -1,33 +1,28 @@
-const { REST, Routes } = require("discord.js");
-const { handleCommands } = require("./commandHandler.js");
+import { REST, Routes } from "discord.js";
+import { handleCommands } from "./commands/command-handler.js";
 
-// Load all commands
+// Load commands
 const commands = handleCommands(null);
 
 // Construct and prepare an instance of the REST module
 const rest = new REST().setToken(process.env.DISCORD_TOKEN);
 
-// and deploy your commands!
+// Deploy commands
 (async () => {
   try {
-    console.log(
-      `Started refreshing ${commands.length} application (/) commands.`
-    );
+    console.log(`[ INFO ] Deploying ${commands.length} commands.`);
 
     // The put method is used to fully refresh all commands in the guild with the current set
-    const data = await rest.put(
+    await rest.put(
       Routes.applicationGuildCommands(
         process.env.CLIENT_ID,
-        process.env.GUILD_ID
+        process.env.GUILD_ID,
       ),
-      { body: commands }
+      { body: commands },
     );
 
-    console.log(
-      `Successfully reloaded ${data.length} application (/) commands.`
-    );
+    console.log(`[ OK ] Registration complete.`);
   } catch (error) {
-    // And of course, make sure you catch and log any errors!
-    console.error(error);
+    console.error(`[ FAIL ] ${error}`);
   }
 })();
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-const { Client, Events, GatewayIntentBits } = require("discord.js");
-const { handleCommands } = require("./commandHandler.js");
+import { Client, Events, GatewayIntentBits } from "discord.js";
+import { handleCommands } from "./commands/command-handler.js";
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-// Load all commands
+// Load commands
 handleCommands(client);
 
 // Annnounce when the client is ready
 client.once(Events.ClientReady, (c) => {
-  console.log(`Ready! Logged in as ${c.user.tag}`);
+  console.log(`[ OK ] Logged in as ${c.user.tag}`);
 });
 
 // Log in to discord using the token
