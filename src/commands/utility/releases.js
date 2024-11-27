5d8d7c14fba144f405176b1fed0695a74567457d
Arthur Segato <arthursegato@protonmail.com>
2024-11-27 01:18:53 +0000

    Add releases command

---
+++ src/commands/utility/releases.js
@@ -0,0 +1,64 @@
+const { SlashCommandBuilder } = require("discord.js");
+const { repos } = require("./utility.json");
+const { color } = require("bun");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("releases")
+    .setDescription("Show the latest releases of each repo"),
+  async execute(interaction) {
+    const retrieveLatestRelease = async (repoUrl) => {
+      try {
+        const response = await fetch(repoUrl, {
+          method: "HEAD",
+        });
+
+        // Abort in case of a error
+        if (!response.ok) throw new Error(response.statusText);
+
+        // Return the url of the latest release
+        return response.url;
+      } catch (err) {
+        console.error(
+          `[ FAIL ]${color("white", "ansi")} ${err.message} when trying to create a measurement `,
+        );
+      }
+    };
+
+    try {
+      // Create measurement requests for all services and wait for the IDs
+      const releasesUrl = await Promise.all(repos.map(retrieveLatestRelease));
+
+      // Construct embeds for each result
+      const embeds = [
+        {
+          title: "Repositories",
+          fields: releasesUrl.flatMap((repo) => [
+            {
+              name: "Project",
+              value: repo.match(/\/([^\/]+)\/([^\/]+)\/releases/)[2],
+              inline: true,
+            },
+            {
+              name: "Version",
+              value: repo.match(/\/tag\/(.*)/)[1],
+              inline: true,
+            },
+            {
+              name: "",
+              value: "",
+              inline: false,
+            },
+          ]),
+          // Give the embed a discord compatible timestamp
+          timestamp: new Date().toISOString(),
+        },
+      ];
+
+      await interaction.reply({ embeds });
+    } catch (err) {
+      console.error(`[ FAIL ]${color("white", "ansi")} ${err.message}`);
+      await interaction.editReply(err.message);
+    }
+  },
+};
